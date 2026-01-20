a981a91cadf2142ddfb1a587a1908f37bfc0978a
Edna Martins <ednamartins.work@gmail.com>
2026-01-20 17:46:31 -0300

    feat: display footer

--- js-intro-convert/scripts.js
+++ js-intro-convert/scripts.js
@@ -6,6 +6,7 @@ const GBP = 7.23
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 amount.addEventListener("input", () => {
    const hasCharactersRegex = /\D+/g
@@ -29,5 +30,11 @@ amount.addEventListener("input", () => {
   }
 
   function convertCurrency(amount, price, symbol) {
-    console.log(amount, price, symbol)
+    try {
+      footer.classList.add("show-result")
+    } catch (error){
+      footer.classList.remove("show-result")
+      console.log(error)
+      alert("Não foi possível converte, tente mais tarde.")
+    }
   }
