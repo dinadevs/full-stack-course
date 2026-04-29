6633600764581d58e482986428a369c577f441db
Edna Martins <ednamartins.work@gmail.com>
2026-04-29 10:15:38 -0300

    feat: create li

--- js-intermediary-refund/scripts.js
+++ js-intermediary-refund/scripts.js
@@ -1,7 +1,7 @@
-const form = document.querySelector("form")
-const amount = document.getElementById("amount")
-const expense = document.getElementById("expense")
-const category = document.getElementById("category")
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -14,13 +14,13 @@ amount.oninput = () => {
 function formatCurrencyUSA(value) {
   return Number(value).toLocaleString("en-US", {
     style: "currency",
-    currency: "USD"
+    currency: "USD",
   });
 }
 
 form.onsubmit = (event) => {
-  event.preventDefault()
-  
+  event.preventDefault();
+
   const newExpense = {
     id: new Date().getTime(),
     expense: expense.value,
@@ -28,14 +28,15 @@ form.onsubmit = (event) => {
     category_name: category.options[category.selectedIndex].text,
     amount: amount.value,
     created_at: new Date(),
-  }
-}
+  };
+};
 
 function expenseAdd(newExpense) {
   try {
-    
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
   } catch (error) {
-    alert("It was not possible to update the list of expenses.")
-    console.log(error)
+    alert("It was not possible to update the list of expenses.");
+    console.log(error);
   }
 }
