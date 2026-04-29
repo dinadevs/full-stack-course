367eda12cab91f70bb8ca2025f02d32db206f316
Edna Martins <ednamartins.work@gmail.com>
2026-04-29 09:57:35 -0300

    feat: format currency

--- js-intermediary-refund/scripts.js
+++ js-intermediary-refund/scripts.js
@@ -3,7 +3,16 @@ const amount = document.getElementById("amount")
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
 
+  value = Number(value) / 100;
+
   amount.value = formatCurrencyUSA(value);
 };
+
+function formatCurrencyUSA(value) {
+  return Number(value).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+}
 
 
