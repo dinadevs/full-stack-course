e7d072b927226d3c0a59c1d822b3e86efa43559d
Edna Martins <ednamartins.work@gmail.com>
2026-05-22 10:37:25 -0300

    feat: register a new appointment in the API.

--- js-hairday/src/modules/form/submit.js
+++ js-hairday/src/modules/form/submit.js
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import {scheduleNew} from "../../services/schedule-new.js"
 
 const form = document.querySelector("form");
 const clientName = document.getElementById("client");
@@ -9,7 +10,7 @@ const inputToday = dayjs().format("YYYY-MM-DD");
 selectedDate.value = inputToday;
 selectedDate.min = inputToday;
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   event.preventDefault();
 
   try {
@@ -30,7 +31,7 @@ form.onsubmit = (event) => {
     
     const id = new Date().getTime()
 
-    console.log({
+    await scheduleNew ({
       id,
       name,
       when,
