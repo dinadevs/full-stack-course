c80d866d044f02374c6205e734199729a3ca066c
Edna Martins <ednamartins.work@gmail.com>
2026-05-22 07:15:00 -0300

    feat: sends selected time to the API

--- js-hairday/src/modules/form/submit.js
+++ js-hairday/src/modules/form/submit.js
@@ -1,13 +1,43 @@
-import dayjs from "dayjs"
+import dayjs from "dayjs";
 
-const form = document.querySelector("form")
-const selectedDate = document.getElementById("date")
+const form = document.querySelector("form");
+const clientName = document.getElementById("client");
+const selectedDate = document.getElementById("date");
 
-const inputToday = dayjs().format("YYYY-MM-DD")
+const inputToday = dayjs().format("YYYY-MM-DD");
 
-selectedDate.value = inputToday
-selectedDate.min = inputToday
+selectedDate.value = inputToday;
+selectedDate.min = inputToday;
 
 form.onsubmit = (event) => {
-  event.preventDefault()
-}
+  event.preventDefault();
+
+  try {
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Please provide the time.");
+    }
+
+    const hourSelected = document.querySelector(".hour-selected");
+    if (!hourSelected) {
+      return alert("Please provide the customer's name.");
+    }
+
+    const[hour] = hourSelected.innerText.split(":")
+
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+    
+    const id = new Date().getTime()
+
+    console.log({
+      id,
+      name,
+      when,
+    })
+
+  } catch (error) {
+    alert("It was not possible to schedule the appointment.");
+    console.log(error);
+  }
+};
