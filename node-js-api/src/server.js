7794330bdbd68d50c92bc2be2b7cab61bf7b04ea
Edna Martins <ednamartins.work@gmail.com>
2026-06-17 07:54:30 -0300

    feat: retrieves data n adds middleware

--- node-js-api/src/server.js
+++ node-js-api/src/server.js
@@ -1,17 +1,19 @@
 import http from "node:http";
+import {jsonBodyHandler} from "./middlewares/jsonBodyHandler.js"
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
+
+  await jsonBodyHandler(request, response)
 
   if (method === "GET" && url === "/products") {
     return response.end("Product list!");
   }
 
   if (method === "POST" && url === "/products") {
-    return response.writeHead(201).end("Registered product!");
+    return response.writeHead(201).end(JSON.stringify(request.body));
   }
 
   return response.writeHead(404).end("Route not found!");
-
 });
 server.listen(3333);
